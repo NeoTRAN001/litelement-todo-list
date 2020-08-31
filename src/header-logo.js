a3919c2d7facf2744c8ae47ae24fce627e6d16a5
NeoTRAN001 <neo_tran@outlook.com>
2020-08-31 17:38:33 -0500

    Header ToDo list, logo and text

---
+++ src/header-logo.js
@@ -0,0 +1,31 @@
+import { LitElement, html, css } from 'lit-element';
+
+export class HeaderLogo extends LitElement {
+    static get styles() {
+        return css`
+            :host {
+                display: block;
+            }
+  
+            h2 {
+                font-family: Arial, Helvetica, sans-serif;
+                color: white;
+            }
+  
+            img {
+                width: 100px;
+            }
+        `;
+    }
+
+    render() {
+        return html`
+            <center>
+                <img src="https://raw.githubusercontent.com/NeoTRAN001/polymer-todo-list-firebase/master/img/ico-logo.png" alt="">
+                <h2>FrontEnd Test LitElement</h2>
+            </center>
+        `;
+    }
+}
+
+customElements.define('header-logo', HeaderLogo);
